50120faaf4a327acf1e3717e10cafc8d150dae5b
azad71 <mdmamun732@gmail.com>
2023-06-07 23:01:47 +0600

    added tailwind css

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1>HELLO WORLD</h1>
+      <h1 className="text-blue-400 text-7xl">HELLO WORLD</h1>
     </main>
   );
 }
