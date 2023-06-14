4237967c162241cb63c77d39c919901f3c9ebe91
azad71 <mdmamun732@gmail.com>
2023-06-14 22:53:52 +0600

    search page made dynamic

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,3 +1,4 @@
+import { PrismaClient } from "@prisma/client";
 import Header from "../components/Header";
 import RestaurantCard from "./components/RestaurantCard";
 import SearchSideBar from "./components/SearchSideBar";
@@ -7,14 +8,52 @@ export const metadata: Metadata = {
   title: "Search Restaurants",
 };
 
-export default function Search() {
+const prisma = new PrismaClient();
+
+const getRestaurantsByLocation = async (city: string) => {
+  const select = {
+    id: true,
+    name: true,
+    main_image: true,
+    price: true,
+    cuisine: true,
+    location: true,
+    slug: true,
+  };
+  if (!city) return prisma.restaurant.findMany({ select });
+
+  return prisma.restaurant.findMany({
+    where: {
+      location: {
+        name: {
+          equals: city.toLowerCase(),
+        },
+      },
+    },
+    select,
+  });
+};
+
+export default async function Search({
+  searchParams,
+}: {
+  searchParams: { city: string };
+}) {
+  const restaurants = await getRestaurantsByLocation(searchParams.city);
+
   return (
     <>
       <Header />
       <div className="flex py-4 m-auto w-2/3 justify-between items-start">
         <SearchSideBar />
         <div className="w-5/6">
-          <RestaurantCard />
+          {restaurants.length ? (
+            restaurants.map((restaurant) => (
+              <RestaurantCard key={restaurant.id} restaurant={restaurant} />
+            ))
+          ) : (
+            <p>Sorry, we found no restaurants in this area</p>
+          )}
         </div>
       </div>
     </>
