e620e6d21640f41ef84d9e81b27a9c850109267a
azad71 <mdmamun732@gmail.com>
2023-07-03 00:02:36 +0600

    added jwt verify middleware

--- pages/api/auth/userInfo.ts
+++ pages/api/auth/userInfo.ts
@@ -2,6 +2,11 @@ import { NextApiRequest, NextApiResponse } from "next";
 import * as jose from "jose";
 import db from "../../../app/lib/db";
 
+export interface IPayload {
+  email: string;
+  exp: number;
+}
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
@@ -9,29 +14,15 @@ export default async function handler(
   if (req.method !== "GET")
     return res.status(404).json({ message: "Invalid routes" });
 
-  const bearerToken = req.headers["authorization"] as string;
-
-  if (!bearerToken) {
-    return res.status(401).json({
-      errorMessage: "Unauthorized request",
-    });
-  }
-
-  const token = bearerToken.split(" ")[1];
+  const token = req.headers["authorization"]?.split(" ")[1] as string;
   const secret = new TextEncoder().encode(process.env.JWT_SECRET);
 
-  let data: any;
-  try {
-    data = await jose.jwtVerify(token, secret);
-  } catch (error) {
-    return res.status(401).json({
-      errorMessage: "Unauthorized request",
-    });
-  }
+  const data = await jose.jwtVerify(token, secret);
+  const payload = data.payload as unknown as IPayload;
 
   const user = await db.user.findUnique({
     where: {
-      email: data.payload.email,
+      email: payload.email,
     },
     select: {
       id: true,
