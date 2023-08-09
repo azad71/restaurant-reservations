3047216bc4f9233f955f622404eb21a9942b2512
azad71 <mdmamun732@gmail.com>
2023-08-10 00:37:49 +0600

    added reservation module

--- app/reserve/[slug]/page.tsx
+++ app/reserve/[slug]/page.tsx
@@ -1,17 +1,43 @@
 import { Metadata } from "next";
 import Form from "./components/Form";
 import Header from "./components/Header";
+import db from "../../lib/db";
+import { notFound } from "next/navigation";
 
-export const metadata: Metadata = {
-  title: "Reserve | Milestone Grill",
+interface Props {
+  params: {
+    slug: string;
+  };
+  searchParams: {
+    date: string;
+    partySize: string;
+  };
+}
+
+const fetchRestaurantBySlug = async (slug: string) => {
+  const restaurant = await db.restaurant.findUnique({ where: { slug } });
+
+  if (!restaurant) notFound();
+
+  return restaurant;
 };
 
-export default function Reserve() {
+export default async function Reserve({ params, searchParams }: Props) {
+  const restaurant = await fetchRestaurantBySlug(params.slug);
   return (
     <div className="border-t h-screen">
       <div className="py-9 w-3/5 m-auto">
-        <Header />
-        <Form />
+        <Header
+          image={restaurant.main_image}
+          name={restaurant.name}
+          date={searchParams.date}
+          partySize={searchParams.partySize}
+        />
+        <Form
+          slug={params.slug}
+          date={searchParams.date}
+          partySize={searchParams.partySize}
+        />
       </div>
     </div>
   );
--- pages/api/restaurant/[slug]/reserve.ts
+++ pages/api/restaurant/[slug]/reserve.ts
@@ -1,146 +1,148 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import db from "../../../../app/lib/db";
 import { findAvailabileTables } from "../../../../services/restaurant/findAvailableTables";
-
-interface ITableCount {
-  [key: number]: number[];
-}
+import db from "../../../../app/lib/db";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.method !== "POST") {
-    return res.status(404).send("Invalid route");
-  }
-
-  const { slug, day, time, partySize } = req.query as Record<string, string>;
-
-  const {
-    bookerEmail,
-    bookerPhone,
-    bookerFirstName,
-    bookerLastName,
-    bookerOccassion,
-    bookerRequest,
-  } = req.body;
-
-  const restaurant = await db.restaurant.findUnique({
-    where: { slug },
-    select: {
-      tables: true,
-      open_time: true,
-      close_time: true,
-      id: true,
-    },
-  });
-
-  if (!restaurant) {
-    return res.status(400).json({
-      errorMessage: "Restaurant not found",
-    });
-  }
-
-  const isInvalidTime =
-    new Date(`${day}T${time}`) < new Date(`${day}T${restaurant.open_time}`) ||
-    new Date(`${day}T${time}`) > new Date(`${day}T${restaurant.close_time}`);
-
-  if (isInvalidTime) {
-    return res.status(400).json({
-      errorMessage: "Restaurant is not open at that time",
-    });
-  }
-
-  const searchTimesWithTables = await findAvailabileTables({
-    time,
-    day,
-    res,
-    restaurant,
-  });
-
-  if (!searchTimesWithTables) {
-    return res.status(400).json({
-      errorMessage: "Invalid data provided",
-    });
-  }
-
-  const foundSearchTime = searchTimesWithTables.find(
-    (table) =>
-      table.date.toISOString() === new Date(`${day}T${time}`).toISOString()
-  );
-
-  if (!foundSearchTime) {
-    return res.status(400).json({
-      errorMessage: "Schedule not available",
-    });
-  }
-
-  const tableCount: ITableCount = {
-    2: [],
-    4: [],
-  };
-
-  foundSearchTime.tables.forEach((table) => {
-    if (table.seats === 2) {
-      tableCount[2].push(table.id);
-    } else {
-      tableCount[4].push(table.id);
-    }
-  });
-
-  const tablesToBook: number[] = [];
-  let seatsRemaining = parseInt(partySize);
-
-  while (seatsRemaining > 0) {
-    if (seatsRemaining >= 3) {
-      if (tableCount[4].length) {
-        tablesToBook.push(tableCount[4][0]);
-        tableCount[4].shift();
-        seatsRemaining -= 4;
-      } else {
-        tablesToBook.push(tableCount[2][0]);
-        tableCount[2].shift();
-        seatsRemaining -= 2;
-      }
-    } else {
-      if (tableCount[2].length) {
-        tablesToBook.push(tableCount[2][0]);
-        tableCount[2].shift();
-        seatsRemaining -= 2;
-      } else {
-        tablesToBook.push(tableCount[4][0]);
-        tableCount[4].shift();
-        seatsRemaining -= 4;
-      }
-    }
-  }
-
-  const booking = await db.booking.create({
-    data: {
-      number_of_people: parseInt(partySize),
-      booking_time: new Date(`${day}T${time}`),
-      booker_email: bookerEmail,
-      booker_phone: bookerPhone,
-      booker_first_name: bookerFirstName,
-      booker_last_name: bookerLastName,
-      restaurant_id: restaurant.id,
-      booker_occasion: bookerOccassion,
-      booker_request: bookerRequest,
-    },
-  });
-
-  const bookingsOnTableData = tablesToBook.map((table_id) => {
-    return {
-      table_id,
-      booking_id: booking.id,
+  if (req.method === "POST") {
+    const { slug, day, time, partySize } = req.query as {
+      slug: string;
+      day: string;
+      time: string;
+      partySize: string;
     };
-  });
 
-  await db.bookingsOnTable.createMany({
-    data: bookingsOnTableData,
-  });
+    const {
+      bookerEmail,
+      bookerPhone,
+      bookerFirstName,
+      bookerLastName,
+      bookerOccasion,
+      bookerRequest,
+    } = req.body;
 
-  return res.json(booking);
+    const restaurant = await db.restaurant.findUnique({
+      where: {
+        slug,
+      },
+      select: {
+        tables: true,
+        open_time: true,
+        close_time: true,
+        id: true,
+      },
+    });
+
+    if (!restaurant) {
+      return res.status(400).json({
+        errorMessage: "Restaurant not found",
+      });
+    }
+
+    if (
+      new Date(`${day}T${time}`) < new Date(`${day}T${restaurant.open_time}`) ||
+      new Date(`${day}T${time}`) > new Date(`${day}T${restaurant.close_time}`)
+    ) {
+      return res.status(400).json({
+        errorMessage: "Restaurant is not open at that time",
+      });
+    }
+
+    const searchTimesWithTables = await findAvailabileTables({
+      day,
+      time,
+      res,
+      restaurant,
+    });
+
+    if (!searchTimesWithTables) {
+      return res.status(400).json({
+        errorMessage: "Invalid data provided",
+      });
+    }
+
+    const searchTimeWithTables = searchTimesWithTables.find((t) => {
+      return t.date.toISOString() === new Date(`${day}T${time}`).toISOString();
+    });
+
+    if (!searchTimeWithTables) {
+      return res.status(400).json({
+        errorMessage: "No availablity, cannot book",
+      });
+    }
+
+    const tablesCount: {
+      2: number[];
+      4: number[];
+    } = {
+      2: [],
+      4: [],
+    };
+
+    searchTimeWithTables.tables.forEach((table) => {
+      if (table.seats === 2) {
+        tablesCount[2].push(table.id);
+      } else {
+        tablesCount[4].push(table.id);
+      }
+    });
+
+    const tablesToBooks: number[] = [];
+    let seatsRemaining = parseInt(partySize);
+
+    while (seatsRemaining > 0) {
+      if (seatsRemaining >= 3) {
+        if (tablesCount[4].length) {
+          tablesToBooks.push(tablesCount[4][0]);
+          tablesCount[4].shift();
+          seatsRemaining = seatsRemaining - 4;
+        } else {
+          tablesToBooks.push(tablesCount[2][0]);
+          tablesCount[2].shift();
+          seatsRemaining = seatsRemaining - 2;
+        }
+      } else {
+        if (tablesCount[2].length) {
+          tablesToBooks.push(tablesCount[2][0]);
+          tablesCount[2].shift();
+          seatsRemaining = seatsRemaining - 2;
+        } else {
+          tablesToBooks.push(tablesCount[4][0]);
+          tablesCount[4].shift();
+          seatsRemaining = seatsRemaining - 4;
+        }
+      }
+    }
+
+    const booking = await db.booking.create({
+      data: {
+        number_of_people: parseInt(partySize),
+        booking_time: new Date(`${day}T${time}`),
+        booker_email: bookerEmail,
+        booker_phone: bookerPhone,
+        booker_first_name: bookerFirstName,
+        booker_last_name: bookerLastName,
+        booker_occasion: bookerOccasion,
+        booker_request: bookerRequest,
+        restaurant_id: restaurant.id,
+      },
+    });
+
+    const bookingsOnTablesData = tablesToBooks.map((table_id) => {
+      return {
+        table_id,
+        booking_id: booking.id,
+      };
+    });
+
+    await db.bookingsOnTables.createMany({
+      data: bookingsOnTablesData,
+    });
+
+    return res.json(booking);
+  }
 }
 
 // http://localhost:3000/api/restaurant/vivaan-fine-indian-cuisine-ottawa/reserve?day=2023-02-03&time=15:00:00.000Z&partySize=8
